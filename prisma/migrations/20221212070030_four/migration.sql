b797aa358dc767256febd3053b9b60fce718954d
shabni <ahsanahmad@gmail.com>
2022-12-12 14:59:55 +0500

    role_id issue resolved

--- prisma/migrations/20221212070030_four/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE `user` ADD COLUMN `address` VARCHAR(191) NULL,
-    ADD COLUMN `phone` INTEGER NULL;
--- prisma/migrations/20221212095644_one/migration.sql
+++ prisma/migrations/20221212095644_one/migration.sql
@@ -20,15 +20,20 @@ CREATE TABLE `User` (
     `fatherName` VARCHAR(191) NULL,
     `joining_date` BIGINT NOT NULL,
     `nic` VARCHAR(191) NOT NULL,
-    `role_Id` VARCHAR(191) NULL,
+    `boss1` VARCHAR(191) NULL,
+    `boss2` VARCHAR(191) NULL,
+    `boss3` VARCHAR(191) NULL,
+    `is_LoggedIn` BOOLEAN NOT NULL DEFAULT false,
+    `is_CheckedIn` BOOLEAN NOT NULL DEFAULT false,
+    `role_id` VARCHAR(191) NULL,
     `emailOffice` VARCHAR(191) NULL,
-    `emailpersonal` VARCHAR(191) NULL,
+    `address` VARCHAR(191) NULL,
+    `phone` INTEGER NULL,
     `date_updated` BIGINT NOT NULL,
     `date_added` BIGINT NOT NULL,
     `is_deleted` BOOLEAN NOT NULL DEFAULT false,
 
     UNIQUE INDEX `User_userName_key`(`userName`),
-    UNIQUE INDEX `User_role_Id_key`(`role_Id`),
     PRIMARY KEY (`user_id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -47,7 +52,7 @@ CREATE TABLE `Attendence` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `User` ADD CONSTRAINT `User_role_Id_fkey` FOREIGN KEY (`role_Id`) REFERENCES `Roles`(`role_id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `User` ADD CONSTRAINT `User_role_id_fkey` FOREIGN KEY (`role_id`) REFERENCES `Roles`(`role_id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `Attendence` ADD CONSTRAINT `Attendence_user_Id_fkey` FOREIGN KEY (`user_Id`) REFERENCES `User`(`user_id`) ON DELETE RESTRICT ON UPDATE CASCADE;

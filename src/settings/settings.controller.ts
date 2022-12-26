3716a16634c03105b9f775f61dd1dbf19154774c
shabni <ahsanahmad@gmail.com>
2022-12-26 11:50:19 +0500

    edit role implemented

--- src/settings/settings.controller.ts
+++ src/settings/settings.controller.ts
@@ -1,14 +1,24 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors, UploadedFile, Res } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseInterceptors,
+  UploadedFile,
+  Res,
+} from '@nestjs/common';
 import { SettingsService } from './settings.service';
 import { CreateRoleDto, CreateSettingDto } from './dto/create-setting.dto';
 import { UpdateSettingDto } from './dto/update-setting.dto';
 import { ApiTags } from '@nestjs/swagger';
 import { FileInterceptor } from '@nestjs/platform-express';
 import * as path from 'path';
-import {diskStorage} from 'multer';
-import { v4 as uuidv4 } from "uuid";
+import { diskStorage } from 'multer';
+import { v4 as uuidv4 } from 'uuid';
 import { join } from 'path';
-
 
 @Controller('api/settings')
 @ApiTags('settings')
@@ -22,7 +32,7 @@ export class SettingsController {
 
   @Get('/getScales')
   getScales() {
-    return {scales:['1','2','3','4','5']};
+    return { scales: ['1', '2', '3', '4', '5'] };
   }
 
   @Get('/getRoles')
@@ -30,31 +40,41 @@ export class SettingsController {
     return this.settingsService.findAllRoles();
   }
 
+  @Get('getRole/:id')
+  findOne(@Param('id') id: string) {
+    return this.settingsService.findOne(id);
+  }
+
+  @Patch('updateRole/:id')
+  updateRole(
+    @Param('id') id: string,
+    @Body() updateSettingDto: UpdateSettingDto,
+  ) {
+    return this.settingsService.updateRole(id, updateSettingDto);
+  }
+
   @Post('/upload')
-  @UseInterceptors(FileInterceptor('photo', {
-    storage: diskStorage({
-      destination: './uploads',
-      filename: (req, file, cb)=>{
-        const filename: string = path.parse(file.originalname).name.replace(/\s/g, '') + uuidv4() ;
-        const extension: string = path.parse(file.originalname).ext;
-        cb(null,`${filename}${extension}`)
-      }
-    })
-  },))
+  @UseInterceptors(
+    FileInterceptor('photo', {
+      storage: diskStorage({
+        destination: './uploads',
+        filename: (req, file, cb) => {
+          const filename: string =
+            path.parse(file.originalname).name.replace(/\s/g, '') + uuidv4();
+          const extension: string = path.parse(file.originalname).ext;
+          cb(null, `${filename}${extension}`);
+        },
+      }),
+    }),
+  )
   uploadSingle(@UploadedFile() file) {
-    
-    let imgUrl = file.path.replace(/\\/g, "/")
-    imgUrl = imgUrl.substring(imgUrl.indexOf('uploads')+8)
-    return {imgUrl}
+    let imgUrl = file.path.replace(/\\/g, '/');
+    imgUrl = imgUrl.substring(imgUrl.indexOf('uploads') + 8);
+    return { imgUrl };
   }
 
   @Get('/getProfileImage/:imgUrl')
-  gindProfileImage( @Res() res, @Param('imgUrl') imgUrl: string) {
-    return res.sendFile(join(process.cwd(), 'uploads/'+imgUrl))
-  }
-
-  @Get('getRole/:id')
-  findOne(@Param('id') id: string){
-    return this.settingsService.findOne(id);
+  gindProfileImage(@Res() res, @Param('imgUrl') imgUrl: string) {
+    return res.sendFile(join(process.cwd(), 'uploads/' + imgUrl));
   }
 }
--- src/settings/settings.service.ts
+++ src/settings/settings.service.ts
@@ -6,40 +6,60 @@ import { UpdateSettingDto } from './dto/update-setting.dto';
 
 @Injectable()
 export class SettingsService {
-
   constructor(private prisma: PrismaService) {}
-  
-  createRole(createRoleDto: CreateRoleDto) {
-    
-    const createdAt = datesForCreate()['date_added']
-    const updatedAt  = datesForCreate()['date_updated']
 
-    return this.prisma.roles.create({data:{
-      role_id: MakeTimedIDUnique(),
-      ...createRoleDto,
-      createdAt,
-      updatedAt
-    }});
+  createRole(createRoleDto: CreateRoleDto) {
+    const createdAt = datesForCreate()['date_added'];
+    const updatedAt = datesForCreate()['date_updated'];
+
+    return this.prisma.roles.create({
+      data: {
+        role_id: MakeTimedIDUnique(),
+        ...createRoleDto,
+        createdAt,
+        updatedAt,
+      },
+    });
   }
 
-  findAllRoles() {
-    return this.prisma.roles.findMany({select:{
-      role_id:true,
-      title: true,
-      scale:true,
-    }});
+  makePermissions(roles) {
+    roles.forEach((element) => {
+      if (element['permissions']) {
+        element['permissions'] = element['permissions'].split(',');
+      }
+    });
+
+    return roles;
+  }
+
+  async findAllRoles() {
+    let roles = await this.prisma.roles.findMany({
+      select: {
+        role_id: true,
+        title: true,
+        scale: true,
+        permissions: true,
+      },
+    });
+
+    return this.makePermissions(roles);
   }
 
   findOne(id: string) {
     return this.prisma.roles.findFirst({
-      where:{
-        role_id:id
-      }
+      where: {
+        role_id: id,
+      },
     });
   }
 
-  update(id: number, updateSettingDto: UpdateSettingDto) {
-    return `This action updates a #${id} setting`;
+  async updateRole(id: string, updateSettingDto: UpdateSettingDto) {
+    const role = await this.prisma.roles.update({
+      where: { role_id: id },
+      data: { ...updateSettingDto },
+    });
+
+    return role;
   }
 
   remove(id: number) {
